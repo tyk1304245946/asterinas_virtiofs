b8fc04d6ed9632fd88b8ea538f3ce120feaef5db
tyk1304245946 <tyk1304245946@outlook.com>
2025-01-15 18:40:46 +0800

    实现interrupt mkdir

--- kernel/comps/virtio/src/device/filesystem/request.rs
+++ kernel/comps/virtio/src/device/filesystem/request.rs
@@ -25,7 +25,9 @@ pub trait AnyFuseDevice {
     fn release(&self, nodeid: u64, fh: u64, flags: u32, lock_owner: u64, flush: bool);
     fn access(&self, nodeid: u64, mask: u32);
     fn statfs(&self, nodeid: u64);
+    fn interrupt(&self, nodeid: u64, unique: u64);
     // fn interrupt(&self, nodeid: u64, fh: u64, lock_owner: u64, unique: u64);
+    fn mkdir(&self, nodeid: u64, mode: u32, umask: u32, name: Vec<u8>);
 }
 
 pub fn fuse_pad_str(name: &str, repr_c: bool) -> Vec<u8> {
